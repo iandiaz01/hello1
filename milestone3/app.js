0ea8aad0e5dde45565145e133305725c8237f116
iandiaz01 <162068409+iandiaz01@users.noreply.github.com>
2024-04-22 21:06:02 -0400

    Update app.js

--- milestone3/app.js
+++ milestone3/app.js
@@ -33,7 +33,7 @@ app.post('/send-email', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3001; 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
